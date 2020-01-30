7f289238b737cab26eda0d4deabf6b78d96015be
chsdwn <hulusikafalier@gmail.com>
2020-01-30 13:07:27 +0300

    Created Features seeder.

---
+++ API/Migrations/20200130100157_SeedFeatures.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('ABS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('GPS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Sunroof')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Self Drive')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Automatic Shift')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name IN ('ABS', 'GPS', 'Sunroof', 'Self Drive', 'Automatic Shift')");
+        }
+    }
+}
