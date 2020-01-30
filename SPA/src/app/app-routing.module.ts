33e4a626403231d5fb0cf6ae4329ebbb47e13483
chsdwn <hulusikafalier@gmail.com>
2020-01-30 12:59:03 +0300

    Created new vehicle form draft. Created base css styles.

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewComponent } from './vehicle/new/new.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: '/vehicle/new'
+  },
+  {
+    path: 'vehicle/new', component: NewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
