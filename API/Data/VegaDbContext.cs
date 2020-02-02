194d9bc75fe41b56e381809a109656195327a78f
chsdwn <hulusikafalier@gmail.com>
2020-02-02 14:06:58 +0300

    Created vehicle feature fluent api and table.

--- API/Data/VegaDbContext.cs
+++ API/Data/VegaDbContext.cs
@@ -9,9 +9,17 @@ namespace API.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(
+                vf => new { vf.FeatureId, vf.VehicleId }
+            );
+        }
+
         public DbSet<Make> Makes { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleFeature> VehicleFeatures { get; set; }
     }
 }
