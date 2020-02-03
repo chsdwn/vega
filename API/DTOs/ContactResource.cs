352be5d2992899371ed383721a4aef3d415a13aa
chsdwn <hulusikafalier@gmail.com>
2020-02-03 11:27:59 +0300

    Updated vehicler creation dto. Created contact dto.

---
+++ API/DTOs/ContactResource.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+    public class ContactResource
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
--- API/DTOs/VehicleForCreation.cs
+++ API/DTOs/VehicleForCreation.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using API.Models;
 
 namespace API.DTOs
@@ -8,15 +9,12 @@ namespace API.DTOs
     {
         public int ModelId { get; set; }
         public bool VehicleRegistered { get; set; }
-        public ICollection<VehicleFeature> VehicleFeatures { get; set; }
-        public string ContactName { get; set; }
-        public string ContactPhone { get; set; }
-        public string ContactEmail { get; set; }
-        public DateTime LastUpdate { get; set; }
+        public ICollection<int> Features { get; set; }
+        public ContactResource Contact { get; set; }
 
         public VehicleForCreation()
         {
-            this.LastUpdate = DateTime.UtcNow;
+            Features = new Collection<int>();
         }
     }
 }
