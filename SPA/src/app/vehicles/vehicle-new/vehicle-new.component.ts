497cfd685a64de4ec23fe31abf6e81f3bad44e4f
chsdwn <hulusikafalier@gmail.com>
2020-02-05 17:59:58 +0300

    Added vehicle creation validators.

--- SPA/src/app/vehicles/vehicle-new/vehicle-new.component.ts
+++ SPA/src/app/vehicles/vehicle-new/vehicle-new.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { VehicleService } from '../../services/vehicle.service';
 
 import { Contact } from './../../models/Contact';
 import { Feature } from './../../models/Feature';
-import { KeyValuePair } from './../../models/KeyValuePair';
 import { Make } from '../../models/Make';
 import { Model } from '../../models/Model';
 import { VehicleCreate } from './../../models/VehicleCreate';
@@ -57,30 +56,29 @@ export class VehicleNewComponent implements OnInit {
   createForm() {
     if (this.editMode) {
       this.initFeatures();
+      this.initModels(this.vehicle.make.id);
 
       this.newVehicleForm = new FormGroup({
         vehicleData: new FormGroup({
-          makes: new FormControl(this.vehicle.make.id),
-          models: new FormControl(this.vehicle.model.id),
-          isCarRegistered: new FormControl(this.vehicle.isRegistered),
-          contactName: new FormControl(this.vehicle.contact.name),
-          contactPhone: new FormControl(this.vehicle.contact.phone),
-          contactMail: new FormControl(this.vehicle.contact.email),
+          makes: new FormControl(this.vehicle.make.id, Validators.required),
+          models: new FormControl(this.vehicle.model.id, Validators.required),
+          isCarRegistered: new FormControl(this.vehicle.isRegistered, Validators.required),
+          contactName: new FormControl(this.vehicle.contact.name, Validators.required),
+          contactPhone: new FormControl(this.vehicle.contact.phone, Validators.required),
+          contactMail: new FormControl(this.vehicle.contact.email, [Validators.required, Validators.email]),
           featureLabels: new FormArray(this.features.map(f => new FormControl(f.selected)))
         })
       });
 
-      this.onMakeChange(this.vehicle.make.id);
-
     } else {
       this.newVehicleForm = new FormGroup({
         vehicleData: new FormGroup({
-          makes: new FormControl(),
-          models: new FormControl(),
-          isCarRegistered: new FormControl(),
-          contactName: new FormControl(),
-          contactPhone: new FormControl(),
-          contactMail: new FormControl(),
+          makes: new FormControl(null, Validators.required),
+          models: new FormControl(null, Validators.required),
+          isCarRegistered: new FormControl(false, Validators.required),
+          contactName: new FormControl(null, Validators.required),
+          contactPhone: new FormControl(null, Validators.required),
+          contactMail: new FormControl(null, [Validators.required, Validators.email]),
           featureLabels: new FormArray(this.features.map(f => new FormControl(false)))
         })
       });
@@ -88,31 +86,33 @@ export class VehicleNewComponent implements OnInit {
   }
 
   onMakeChange(id: number) {
-    if (id) {
-      this.models = this.makes.find(m => m.id === id).models;
-    }
+    this.initModels(id);
+
+    (this.newVehicleForm.controls.vehicleData as FormGroup).patchValue({
+      models: [this.models[0].id]
+    });
   }
 
   onSubmit() {
-    const modelId = +this.newVehicleForm.value.vehicleData.models;
-    const isRegistered = this.newVehicleForm.value.vehicleData.isCarRegistered;
-    const contactName = this.newVehicleForm.value.vehicleData.contactName;
-    const contactEmail = this.newVehicleForm.value.vehicleData.contactMail;
-    const contactPhone = this.newVehicleForm.value.vehicleData.contactPhone;
+    if (this.newVehicleForm.valid) {
+      const modelId = +this.newVehicleForm.value.vehicleData.models;
+      const isRegistered = this.newVehicleForm.value.vehicleData.isCarRegistered;
+      const contactName = this.newVehicleForm.value.vehicleData.contactName;
+      const contactEmail = this.newVehicleForm.value.vehicleData.contactMail;
+      const contactPhone = this.newVehicleForm.value.vehicleData.contactPhone;
 
-    const featureIds: number[] = [];
-    this.features.map(f => f.selected === true ? featureIds.push(f.id) : null);
+      const featureIds: number[] = [];
+      this.features.map(f => f.selected === true ? featureIds.push(f.id) : null);
 
-    const contact = new Contact(contactName, contactPhone, contactEmail);
+      const contact = new Contact(contactName, contactPhone, contactEmail);
 
-    const createVehicle = new VehicleCreate(
-      modelId,
-      isRegistered,
-      featureIds,
-      contact
-    );
-
-    console.log(createVehicle);
+      const createVehicle = new VehicleCreate(
+        modelId,
+        isRegistered,
+        featureIds,
+        contact
+      );
+    }
   }
 
   initFeatures() {
@@ -124,6 +124,12 @@ export class VehicleNewComponent implements OnInit {
           }
         });
       });
+    }
+  }
+
+  initModels(id: number) {
+    if (id) {
+      this.models = this.makes.find(m => m.id === id).models;
     }
   }
 }
