1d82541806d99a52a0201e611e37f87b37a4544e
chsdwn <hulusikafalier@gmail.com>
2020-02-07 18:38:12 +0300

    Removed edit, delete buttons from vehicle list. Created make filter.

--- SPA/src/app/vehicles/vehicle-list/vehicle-list.component.ts
+++ SPA/src/app/vehicles/vehicle-list/vehicle-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
 
 import { VehicleService } from './../../services/vehicle.service';
+import { Make } from './../../models/Make';
 import { VehicleList } from './../../models/VehicleList';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-list',
@@ -10,12 +13,40 @@ import { VehicleList } from './../../models/VehicleList';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: VehicleList[];
+  filteredVehicles: VehicleList[];
+  makes: Make[];
 
-  constructor(private vehicleService: VehicleService) { }
+  constructor(
+    private router: Router,
+    private vehicleService: VehicleService
+  ) { }
 
   ngOnInit() {
-    this.vehicleService.getVehicles().subscribe(data => {
-      this.vehicles = data;
+    forkJoin(
+      this.vehicleService.getVehicles(),
+      this.vehicleService.getMakes()
+    ).subscribe(([vehicles, makes]) => {
+      this.vehicles = vehicles;
+      this.filteredVehicles = vehicles;
+      this.makes = makes;
+    });
+  }
+
+  onVehicleView(id: number) {
+    this.router.navigate(['/vehicles/', id]);
+  }
+
+  onMakeFilterChange(id: number) {
+    if (id === -1) {
+      this.filteredVehicles = this.vehicles;
+      return;
+    }
+
+    this.filteredVehicles = [];
+    this.vehicles.find(v => {
+      if (v.make.id === id) {
+        this.filteredVehicles.push(v);
+      }
     });
   }
 }
