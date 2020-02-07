dd7d5124045e21eee1b1e9f1949b12963e6cf807
chsdwn <hulusikafalier@gmail.com>
2020-02-07 14:41:23 +0300

    Show error method runs in Zone.

--- SPA/src/app/shared/app.error-handler.ts
+++ SPA/src/app/shared/app.error-handler.ts
@@ -1,10 +1,19 @@
-import { ErrorHandler, Inject } from '@angular/core';
+import { ErrorHandler, Inject, NgZone, Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class AppErrorHandler implements ErrorHandler {
-  constructor(@Inject(ToastrService) private toastr: ToastrService) { }
+  constructor(
+    private ngZone: NgZone,
+    @Inject(ToastrService) private toastr: ToastrService
+  ) { }
 
   handleError(error: any): void {
-    this.toastr.error('An error occured!');
+    // This code puts in a zone. When a code runs in Zone, Angular runs it before changed detection.
+    this.ngZone.run(() => {
+      this.toastr.error('An error occured!');
+    });
   }
 }
